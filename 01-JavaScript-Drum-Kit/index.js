7ec1d57865d457c4482e3e6ca2d91c8b95247de2
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-05-11 00:39:07 +0300

    refactor the logic

--- 01-JavaScript-Drum-Kit/index.js
+++ 01-JavaScript-Drum-Kit/index.js
@@ -1,18 +1,16 @@
-const keys = document.querySelectorAll('.key');
-const soundes = document.querySelectorAll('audio');
-
 window.addEventListener('keydown', (e) => {
-  const code = e.keyCode;
-  keys.forEach((key) => {
-    key.classList.remove('playing');
-    if (+key.dataset.key === code) {
-      key.classList.add('playing');
-    }
-  });
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-  soundes.forEach((sound) => {
-    if (+sound.dataset.key === code) {
-      sound.play();
-    }
-  });
+  if (!audio) return;
+  audio.currentTime = 0; // reset the time if there is double click
+  audio.play();
+  key.classList.add('playing');
+});
+
+window.addEventListener('keyup', (e) => {
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if (!key) return;
+  key.classList.remove('playing');
 });
