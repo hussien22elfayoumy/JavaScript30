f42e70ec9ab0067ed6c0f371bd71fceceae275c7
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-05-11 00:03:33 +0300

    add class to indecate the key change

--- 01-JavaScript-Drum-Kit/index.js
+++ 01-JavaScript-Drum-Kit/index.js
@@ -3,6 +3,12 @@ const soundes = document.querySelectorAll('audio');
 
 window.addEventListener('keydown', (e) => {
   const code = e.keyCode;
+  keys.forEach((key) => {
+    key.classList.remove('playing');
+    if (+key.dataset.key === code) {
+      key.classList.add('playing');
+    }
+  });
 
   soundes.forEach((sound) => {
     if (+sound.dataset.key === code) {
