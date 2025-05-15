2b24a5aa336d6d466f9b6093b70f6ce8dc692e2e
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-05-15 21:43:14 +0300

    add funny audio

--- 02-JS-and-CSS-Clock/index.js
+++ 02-JS-and-CSS-Clock/index.js
@@ -1,6 +1,7 @@
 const secHand = document.querySelector('.second-hand');
 const minHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+const audio = document.querySelector('audio');
 displayDate(); // ignore the layout freez when first open the app
 
 setInterval(() => {
@@ -9,6 +10,7 @@ setInterval(() => {
 
 function displayDate() {
   const date = new Date();
+
   const sec = date.getSeconds();
 
   secHand.style.transform = `rotate(${(sec / 60) * 360 + 90}deg)`;
@@ -18,4 +20,8 @@ function displayDate() {
 
   const hour = date.getHours();
   hourHand.style.transform = `rotate(${(hour / 12) * 360 + 90}deg)`;
+  console.log(sec, min, hour);
+  if (hour === 0 || hour === 24) {
+    audio.play();
+  }
 }
