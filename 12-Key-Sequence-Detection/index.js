00ff8732d5574ce596e17650be48c13ab7929fef
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-05-23 23:19:36 +0300

    make it work on phone

--- 12-Key-Sequence-Detection/index.js
+++ 12-Key-Sequence-Detection/index.js
@@ -1,7 +1,8 @@
+const input = document.querySelector('input');
 const pressedKeys = [];
 const magicWord = 'cat';
 
-window.addEventListener('keydown', (e) => {
+/* window.addEventListener('keydown', (e) => {
   pressedKeys.push(e.key);
   pressedKeys.splice(-magicWord - 1, pressedKeys.length - magicWord.length);
   if (pressedKeys.join('').includes(magicWord)) {
@@ -12,4 +13,24 @@ window.addEventListener('keydown', (e) => {
       emojis: ['🐱', '⚡️', '💥', '✨', '💫', '🌸', '🐱', '🐱'],
     });
   }
+}); */
+
+input.addEventListener('input', (e) => {
+  console.log(e.target.value);
+  pressedKeys.push(e.target.value);
+  pressedKeys.splice(
+    -(magicWord.length - 1),
+    pressedKeys.length - magicWord.length
+  );
+  if (pressedKeys.join('').includes(magicWord)) {
+    e.target.value = '';
+    // cornify_add();
+    const jsConfetti = new JSConfetti();
+
+    jsConfetti.addConfetti({
+      emojis: ['🐱', '⚡️', '💥', '✨', '💫', '🌸', '🐱', '🐱'],
+    });
+  }
 });
+
+window.addEventListener('click', () => input.focus());
