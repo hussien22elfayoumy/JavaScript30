a913e30a6da51766111c24320132fcd8a5d65604
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-05-23 22:20:23 +0300

    implement progress bar and audio and video speed

--- 11-Custom-Video-Player/index.js
+++ 11-Custom-Video-Player/index.js
@@ -24,8 +24,23 @@ const updateBtn = function () {
 
 const skip = function () {
   const skipValue = this.dataset.skip;
-  console.log(video.currentTime);
   video.currentTime += +skipValue;
+};
+
+const handleRangeUpdate = function () {
+  video[this.name] = this.value;
+};
+
+const handleProgress = function () {
+  const percent = (video.currentTime / video.duration) * 100;
+
+  progressBar.style.flexBasis = `${percent}%`;
+};
+
+const scrub = function (e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+
+  video.currentTime = scrubTime;
 };
 
 // hook up the listenrs
@@ -33,5 +48,19 @@ toggleBtn.addEventListener('click', togglePlay);
 video.addEventListener('play', updateBtn);
 video.addEventListener('pause', updateBtn);
 video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', handleProgress);
 
 skipButtons.forEach((skipButton) => skipButton.addEventListener('click', skip));
+
+ranges.forEach((range) => range.addEventListener('input', handleRangeUpdate));
+
+progress.addEventListener('click', scrub);
+
+let mouseDown = false;
+progress.addEventListener('mousemove', (e) => {
+  if (!mouseDown) return;
+  scrub(e);
+});
+
+progress.addEventListener('mousedown', () => (mouseDown = true));
+progress.addEventListener('mouseup', () => (mouseDown = false));
