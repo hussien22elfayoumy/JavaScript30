1ccc5807c54e3341e43b53f13e68980fa3447ba9
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-05-23 23:54:16 +0300

    update files

--- 12-Key-Sequence-Detection/index.js
+++ 12-Key-Sequence-Detection/index.js
@@ -2,19 +2,6 @@ const input = document.querySelector('input');
 const pressedKeys = [];
 const magicWord = 'cat';
 
-/* window.addEventListener('keydown', (e) => {
-  pressedKeys.push(e.key);
-  pressedKeys.splice(-magicWord - 1, pressedKeys.length - magicWord.length);
-  if (pressedKeys.join('').includes(magicWord)) {
-    // cornify_add();
-    const jsConfetti = new JSConfetti();
-
-    jsConfetti.addConfetti({
-      emojis: ['🐱', '⚡️', '💥', '✨', '💫', '🌸', '🐱', '🐱'],
-    });
-  }
-}); */
-
 window.addEventListener('click', () => {
   input.focus();
 });
